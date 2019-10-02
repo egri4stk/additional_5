6af7ae5c001246bda992f6255b60d43530d8f88c
Yahor Hrachok <yhrachok@exadel.com>
2019-10-02 18:04:39 +0300

    feat(brackets): method implimentation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const check = (str, bracketsConfig) => {
+  const symbolMap = new Map();
+  const stack = [];
+  bracketsConfig.forEach((configPair, i) => {
+    if(configPair[0] !== configPair[1]) {
+      symbolMap.set(configPair[0], i + 1);
+      symbolMap.set(configPair[1], -(i + 1));
+    } else {
+      symbolMap.set(configPair[1], 0);
+    }
+  });
+
+  str.split('').forEach((symbol, i) => {
+    const symbolValue = symbolMap.get(symbol);
+    if (symbolValue > 0) {
+      stack.push({
+        key: symbol,
+        value: symbolValue,
+      });
+    } else if(symbolValue < 0){
+      last = stack.slice(-1)[0] || {};
+      if (Math.abs(last.value) === Math.abs(symbolValue)) {
+        stack.pop();
+      } else {
+        stack.push({
+          key: symbol,
+          value: symbolValue,
+        })
+      }
+    }
+    else if(symbolValue === 0) {
+      last = stack.slice(-1)[0] || {};
+      if(last.key === symbol) {
+        stack.pop();
+      } else {
+        stack.push(
+          {
+            key: symbol,
+            value: symbolValue,
+          }
+        )
+      }
+    }
+  });
+  return stack.length === 0;
 }
+module.exports = check;
+
